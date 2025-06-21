dbd5d799ff3eaedc9ee173ef13bc273a0529ba00
Deciptikon <deciptikon@mail.ru>
2025-06-21 05:45:09 +0300

    build to docs

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   base: './',
+  build: {
+    outDir: 'docs',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
